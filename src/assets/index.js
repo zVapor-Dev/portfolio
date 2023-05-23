c095a1af86d49640f01cc2b9305ad56d94615c62
zVapor_ <zvapor231@gmail.com>
2023-05-23 18:10:44 +0200

    fixed a bug

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,7 +25,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-import discord from "./company/discord.png";
+import discord from "./company/Discord.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
