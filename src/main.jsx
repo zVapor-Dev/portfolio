65f401f432b0320faf70600a834f44703d9d2c59
zVapor_ <zvapor231@gmail.com>
2024-02-28 14:27:18 +0100

    fixed some issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
